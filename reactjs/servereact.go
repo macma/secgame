80bd02d51df067ef81eb6e4ac7d98d0f0ad9367c
Ma Mac <macma@Mas-MacBook-Pro.local>
2020-09-12 21:38:55 +0800

    integrate with graph

--- reactjs/servereact.go
+++ reactjs/servereact.go
@@ -2,12 +2,13 @@ package reactjs
 
 import (
 	"fmt"
+	"html"
 	"html/template"
 	"net/http"
 )
 
 const (
-	Port = ":8080"
+	Port = ":8081"
 )
 
 func serveStaticClient(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +19,33 @@ func serveStaticClient(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+type Todo struct {
+	Name string
+	uv   int
+	pv   int
+	amt  int
+}
+
+func htemplateHandler(w http.ResponseWriter, r *http.Request) {
+
+	var tmpl = template.Must(template.ParseFiles("../../reactjs/htemplate.html"))
+
+	var todo Todo
+	todo.Name = html.UnescapeString(`var data = [
+  ];
+		  `)
+	// todo.uv = 4000
+	// todo.pv = 2400
+	// todo.amt = 2400
+	// todo.Head = head
+	tmpl.Execute(w, todo)
+
+}
+
 func ServeHTML() {
+	http.Handle("/", http.FileServer(http.Dir("../../reactjs/")))
+
+	http.HandleFunc("/htemplate", htemplateHandler)
 	http.HandleFunc("/ws", serveStaticClient)
 	http.ListenAndServe(Port, nil)
 }
--- reactjs/ws.go
+++ reactjs/ws.go
@@ -3,6 +3,8 @@ package reactjs
 import (
 	"fmt"
 	"game/db"
+	"html"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -54,15 +56,15 @@ func clickWriter(coord *Clicks) {
 
 func blueHandler(w http.ResponseWriter, r *http.Request) {
 	// blueCounter++
-	processClick(1)
+	processClick(1, w, r)
 }
 
 func orangeHandler(w http.ResponseWriter, r *http.Request) {
 	// orangeCounter++
-	processClick(0)
+	processClick(0, w, r)
 }
 
-func processClick(tp int) {
+func processClick(tp int, w http.ResponseWriter, r *http.Request) {
 	if orangeCounter == 0 && blueCounter == 0 {
 		initTime = time.Now()
 	}
@@ -82,8 +84,25 @@ func processClick(tp int) {
 		clicks.bc = blueCounter
 		db.InsertClicks(tp)
 		go clickWriter(&clicks)
-	}
+	} else {
+		var tmpl = template.Must(template.ParseFiles("../../reactjs/htemplate.html"))
 
+		var todo Todo
+		todo.Name = html.UnescapeString(`var data = [
+			{name: 1, uv: 4000, pv: 2400, amt: 2400},
+			{name: 2, uv: 4000, pv: 2400, amt: 2500},
+			{name: 3, uv: 4000, pv: 2400, amt: 2600},
+			{name: 4, uv: 4000, pv: 2400, amt: 2700},
+			{name: 5, uv: 4000, pv: 2400, amt: 2800},
+	  ];
+			  `)
+		todo.uv = 4000
+		todo.pv = 2400
+		todo.amt = 2400
+		// todo.Head = head
+		tmpl.Execute(w, todo)
+
+	}
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -104,7 +123,15 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 func echo() {
 	for {
 		val := <-clickbroadcast
-		latlong := fmt.Sprintf("%d %d", val.bc, val.oc)
+		latlong := fmt.Sprintf("%d@%d@%s", val.bc, val.oc, `[
+			{name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+			{name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+			{name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+			{name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+			{name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+			{name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+			{name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+	  ]`)
 		// send to every client that is currently connected
 		for client := range clients {
 			err := client.WriteMessage(websocket.TextMessage, []byte(latlong))
